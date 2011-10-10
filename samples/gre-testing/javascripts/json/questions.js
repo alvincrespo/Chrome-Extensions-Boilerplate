9ab74aa237d79a39ad2821c1037a76cc5377fcec
Alvin Crespo <alvin.crespo@gmail.com>
2011-10-10 11:35:40 -0400

    began preliminary dev on gre-testing

---
+++ samples/gre-testing/javascripts/json/questions.js
@@ -0,0 +1,29 @@
+var questionObj = {
+	"questions": [
+		{
+			"question": "The average (arithmetic mean) of x and y is 20. If z = 5, what is the average of x, y, and z?",
+			"answers": [ "8-1/3", "10", "12-1/2", "15", "17-1/2" ],
+			"correct": 0
+		},
+		{
+			"question": "The average (arithmetic mean) of x and y is 20. If z = 5, what is the average of x, y, and z?",
+			"answers": [ "8-1/3", "10", "12-1/2", "15", "17-1/2" ],
+			"correct": 1
+		},
+		{
+			"question": "The average (arithmetic mean) of x and y is 20. If z = 5, what is the average of x, y, and z?",
+			"answers": [ "8-1/3", "10", "12-1/2", "15", "17-1/2" ],
+			"correct": 2
+		},
+		{
+			"question": "The average (arithmetic mean) of x and y is 20. If z = 5, what is the average of x, y, and z?",
+			"answers": [ "8-1/3", "10", "12-1/2", "15", "17-1/2" ],
+			"correct": 3
+		},
+		{
+			"question": "The average (arithmetic mean) of x and y is 20. If z = 5, what is the average of x, y, and z?",
+			"answers": [ "8-1/3", "10", "12-1/2", "15", "17-1/2" ],
+			"correct": 4
+		}
+	]
+};
--- samples/gre-testing/javascripts/popup/popup.js
+++ samples/gre-testing/javascripts/popup/popup.js
@@ -1,8 +1,45 @@
 document.onreadystatechange = function () {
 	if (document.readyState == "complete") {
 		
-		//this is the js used in the popup, when the 
-		//browser_action popup is executed
+		var ulEle = document.createElement('ul');
+				
+		for(var i = 0; i < questionObj.questions.length; i++) {
+			var liEle = document.createElement('li');
+			// create the <h2>
+			var questionEle = document.createElement('h2');
+			var questionTextNode = document.createTextNode(questionObj.questions[i].question);
+			questionEle.appendChild(questionTextNode);
+			liEle.appendChild(questionEle);
+			// ----
+			
+			//create the <form>
+			var formEle = document.createElement('form');			
+			
+			//create the <label>
+			var labelEle = document.createElement('label');
+				labelEle.setAttribute('for', 'answer' + [i]);
+			var labelTextNode = document.createTextNode('Select an Answer: ');
+			labelEle.appendChild(labelTextNode);
+			formEle.appendChild(labelEle);
+			
+			//create the <select>
+			var selectEle = document.createElement('select');
+				selectEle.setAttribute('id', 'answer' + [i]);
+			for(var x = 0; x < questionObj.questions[i].answers.length; x++) {
+				var selectOptionEle = document.createElement('option');
+				var selectOptionTextNode = document.createTextNode(questionObj.questions[i].answers[x]);
+				selectOptionEle.appendChild(selectOptionTextNode);
+				selectEle.appendChild(selectOptionEle);
+			}
+			formEle.appendChild(selectEle);
+			
+			liEle.appendChild(formEle);
+			// ----
+			
+			ulEle.appendChild(liEle);
+		}
+		
+		document.body.appendChild(ulEle);
 		
 	}
 };
